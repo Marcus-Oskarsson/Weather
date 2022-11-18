2e57d8ebc4bc66a50d6acd70d78c5f0d5e96b8bd
Marcus <Marcusoskarsson@hotmail.com>
2022-11-18 16:11:00 +0100

    handling city not found

--- src/code/api/weatherApi.js
+++ src/code/api/weatherApi.js
@@ -10,18 +10,45 @@ export async function getWeather(city) {
     if (weather.status !== 200) throw new Error(weather.statusText);
     return onSuccess(weather);
   } catch (error) {
-    // Just nu hinner jag inte generalisera returnha det såhär så att det fungerar med cities till efter inlämningen. Just nu hanteras det som innan i cities.js, kanske rimligt att göra likadant för weather.js?
+    // Just nu hinner jag inte generalisera det så att det fungerar med cities till innan inlämningen. Just nu hanteras det som innan i cities.js, kanske rimligt att göra likadant för weather.js?
     if (location.href.includes("cities.html")) return;
     // Sätter om sparade staden så man inte laddar om och försöker hämta samma fel igen.
-    localStorage.setItem("weather-city", "Göteborg");
-    document.getElementById("city-search").setAttribute("placeholder", error);
+    return onError(error);
   }
 }
 
 export async function getWeatherForecast(city) {
-  return await (await fetch(WEATHER_FORECAST(city))).json();
+  try {
+    let response = await fetch(WEATHER_FORECAST(city));
+    let forecast = await response.json();
+    if (forecast?.cod === "404") throw new Error(forecast?.message);
+    return await forecast;
+  } catch (error) {
+    return { list: [] };
+  }
 }
 
 async function onSuccess(weather) {
   return await weather.json();
+}
+
+function onError(error) {
+  localStorage.setItem("weather-city", "Göteborg");
+  document
+    .getElementById("city-search")
+    .setAttribute("placeholder", String(error).slice(7));
+  return {
+    name: "Terra incognita",
+    main: {
+      temp: 0,
+      feels_like: 0,
+    },
+    weather: [
+      {
+        main: "unknown",
+        description: "Here be dragons",
+        icon: "None",
+      },
+    ],
+  };
 }
