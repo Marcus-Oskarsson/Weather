e63abbd5b68f474a1661e8a1c0164224c43f1ca5
Marcus <marcusoskarsson@hotmail.com>
2022-11-21 22:27:45 +0100

    removed code not related to weather to new repository

--- src/code/hamburger.js
+++
@@ -1,9 +0,0 @@
-import { addClickActionToElement, getElementById } from "./helperFunctions.js";
-
-let hamburger = getElementById("hamburger");
-addClickActionToElement(hamburger)(toggleHamburgerMeny);
-
-function toggleHamburgerMeny() {
-  let links = document.querySelector("nav");
-  links.classList.toggle("nav-active");
-}
