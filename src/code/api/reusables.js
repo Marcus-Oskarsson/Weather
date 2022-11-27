b72ce4f8bc164be527beceb3f4ed8e17b80e5bfd
Marcus <marcusoskarsson@hotmail.com>
2022-11-27 13:16:56 +0100

    remove unused

--- src/code/api/reusables.js
+++ src/code/api/reusables.js
@@ -1,9 +1,4 @@
-export const API_TRIVIA_CATEGORIES = "https://opentdb.com/api_category.php";
-export const API_TRIVIA_BASE_URL = "https://opentdb.com/api.php?";
-export const API_TRIVIA_SESSION_URL =
-  "https://opentdb.com/api_token.php?command=request";
-export const API_URL_CITIES = "https://avancera.app/cities";
-const WEATHER_API_KEY = "3340cbe473b3001d4487c919d349bee2";
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 export function API_URL_WEATHER_TODAY(city) {
   return `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&APPID=${WEATHER_API_KEY}`;
